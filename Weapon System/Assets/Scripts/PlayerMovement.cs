3af97bc6e5cffb4091f7811f7e41165af921a96a
alexanderkhasaev <alexanderkhasaev@gmail.com>
2022-12-06 23:31:20 +0300

    WeaponSway

--- Weapon System/Assets/Scripts/PlayerMovement.cs
+++ Weapon System/Assets/Scripts/PlayerMovement.cs
@@ -4,17 +4,41 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public CharacterController controller;
+    [SerializeField] private CharacterController controller;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundMask;
 
-    public float speed;
+    [SerializeField] private float speed;
+    [SerializeField] private float gravity;
+    [SerializeField] private float jumpHeight;
+    [SerializeField] private float groundDistance;
+
+    private Vector3 _velocity;
+    private bool _isGrounded;
         
     void Update()
     {
+        _isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(_isGrounded && _velocity.y < 0)
+        {
+            _velocity.y = -2f;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && _isGrounded)
+        {
+            _velocity.y += Mathf.Sqrt(jumpHeight * -2f * gravity); 
+        }
+
+        _velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(_velocity * Time.deltaTime);
     }
 }
---
+++ Weapon System/Assets/Scripts/WeaponSway.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSway : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float sensitivityMultiplier;
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * sensitivityMultiplier;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * sensitivityMultiplier;
+
+        Quaternion rotationX = Quaternion.AngleAxis(-mouseY, Vector3.right);
+        Quaternion rotationY = Quaternion.AngleAxis(mouseX, Vector3.up);
+
+        Quaternion targetRotation = rotationX * rotationY;
+
+        transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, speed * Time.deltaTime);
+    }
+}
