c5546863f77403b563ea068e42b4de570453116c
alexanderkhasaev <alexanderkhasaev@gmail.com>
2022-12-06 22:25:09 +0300

    PlayerMovement - 1

--- Weapon System/Assets/Scripts/MouseLook.cs
+++ Weapon System/Assets/Scripts/MouseLook.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
-    public float mouseSensitivity = 100f;
+    public float mouseSensitivity;
     public Transform playerBody;
+
+    private float xRotation;
 
     void Start()
     {
+        xRotation = 0f;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
@@ -16,11 +20,12 @@ public class MouseLook : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        //xRotation -= mouseY;
-        //xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        //transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
 
         playerBody.Rotate(Vector3.up * mouseX);
+
     }
 }
